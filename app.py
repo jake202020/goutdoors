d9604e18f5cedb4d32f3f8bc7e90ad973b33f429
jake <slepjake@gmail.com>
2020-08-24 15:46:43 -0400

    homepage structure and information

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Capstone 1: National Parks Site and Journal"""
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+import requests
 
 app = Flask(__name__)
 
@@ -15,5 +16,9 @@ debug = DebugToolbarExtension(app)
 @app.route("/")
 def show_home():
     """Homepage to show information about site"""
+    # parks = requests.get("https://developer.nps.gov/api/v1/parks?api_key=vG3tRs4GwEF0x8qK9FmVp5h8AhZjbkH13FydMyTc")
+    # parks_json = parks.json()
+    # parks_data = parks_json["data"]
 
+    # return render_template("index.html", parks=parks_data)
     return render_template("index.html")
