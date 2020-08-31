d8f54666a74a7083ad5c4ed38157f982558e1cc5
jake <slepjake@gmail.com>
2020-08-31 17:12:23 -0400

    add to user tests

--- tests.py
+++ tests.py
@@ -29,6 +29,7 @@ class UserModelTestCase(TestCase):
 
         u1 = User.register("test", "password", "email@email.com", "test", "case")
 
+        db.session.add(u1)
         db.session.commit()
 
         u1 = User.query.get("test")
@@ -42,7 +43,7 @@ class UserModelTestCase(TestCase):
         return res
     
     def test_user_model(self):
-        """Does the user model work?"""
+        """Does the basic user model work?"""
         
         u = User(
             username="test1",
@@ -58,8 +59,47 @@ class UserModelTestCase(TestCase):
         # user state code should be blank
         self.assertIsNone(u.state_code)
 
-        # user name should be test1
+    def test_valid_signup(self):
+        """Does the register method work?"""
+        u = User.register("test1","password","test@email.com","test","user")
+
+        db.session.add(u)
+        db.session.commit()
+
+        u = User.query.get("test1")
+
+        self.assertIsNotNone(u)
         self.assertEqual(u.username, "test1")
+        self.assertEqual(u.email, "test@email.com")
+        self.assertEqual(u.first_name, "test")
+        self.assertEqual(u.last_name, "user")
+        self.assertNotEqual(u.password, "password")
+        # Bcrypt password should start with $2b$
+        self.assertTrue(u.password.startswith("$2b$"))
+
+    ######################
+    # Journal Tests
+    ######################
+    def test_journal_model(self):
+        """Does the basic journal model work?"""
+
+    def test_journal_view(self):
+        """Does the html show up for a journal"""
+
+    ######################
+    # Authentication Tests
+    #####################
+
+    def test_valid_authentication(self):
+        u = User.authenticate(self.u1.username, "password")
+        self.assertIsNotNone(u)
+        self.assertEqual(u.username, self.u1.username)
+    
+    def test_invalid_username(self):
+        self.assertFalse(User.authenticate("badusername", "password"))
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.u1.username, "badpassword"))
 
 # class UserViewTestCase(TestCase):
 #     """Test views for users"""
